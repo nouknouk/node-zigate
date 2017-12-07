3b24a1297193a9ce701cfff19988953c966d1ab4
nouknouk <nouknouk@gmail.com>
2017-12-08 00:42:02 +0100

    updated test sample app to automatically guess Zigate port.

--- test/index.js
+++ test/index.js
@@ -1,12 +1,4 @@
-if (process.argv.length < 2 || !process.argv[2] || process.argv[2]==='-h' || process.argv[2]==='--help' ) {
-  console.log("You must provide the path to the serial port of the Zigate key.")
-  console.log("Examples:");
-  console.log("    node test /dev/ttyUSB0     (linux)");
-  console.log("    node test COM3             (windows)");
-  process.exit(0);
-}
-
-const port=process.argv[2];
+const port=process.argv.length >= 3 ? process.argv[2] : null;
 
 let Zigate = require(__dirname+'/..');
 
@@ -14,31 +6,32 @@ let myZikey = new Zigate();
 
 myZikey.on('open', function(err) {
   if (err) {
-    console.error("connection failed: ",err);
+    console.error("[test] connexion failed: ",err);
   }
   else {
-    console.log("connection to Zigate well established.");
+    console.log("[test] connexion to Zigate well established.");
 
     myZikey.send("get_version");
-    console.log("command 'get_version' sent ; awaiting response...");
+    console.log("[test] command 'get_version' sent ; awaiting response...");
   }
 });
 
 myZikey.on('close', function() {
-  console.error("connection to Zigate closed. Exiting.");
+  console.error("[test] connexion to Zigate closed. Exiting.");
   process.exit(0);
 });
 
 myZikey.on('error', function(err) {
-  console.error("error: ",err);
+  console.error("[test] error: ",err);
 });
 
 myZikey.on('response', function(response) {
-  console.log("response '"+response.type.name+"' received: ", JSON.stringify(response));
+  console.log("[test] response '"+response.type.name+"' received: ", JSON.stringify(response));
   myZikey.close();
-  console.log("closing connexion...");
+  console.log("[test] closing connexion...");
 });
 
 
-console.log("opening connexion to port '"+port+"'...");
+if (port) console.log("[test] opening connexion to port '"+port+"'...");
+else console.log("[test] attempt to guess the Zigate port before connecting...");
 myZikey.open(port);
