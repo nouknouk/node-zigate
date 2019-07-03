0ac18a0c9831d8000613fd125846bd083ef2e1fc
noukonuk <nouknouk@gmail.com>
2019-07-03 07:55:52 +0200

    fixed typo

--- src/driver/responses/time_server.js
+++ src/driver/responses/time_server.js
@@ -3,6 +3,6 @@ module.exports = {
 	name: "time_server",
 	parse: function(reader, rep) {
 		rep.timestamp = reader.nextUInt32BE();
-		rep.time = new Date(rep.timestamp *1000 + (new Date("2000-01-01T00:00:00.000Z").getTime());
+		rep.time = new Date(rep.timestamp *1000 + (new Date("2000-01-01T00:00:00.000Z")).getTime());
 	},
 };
